b419d0e34427eef8747e366dfd51d5e9e126ae81
Ramprakash Ram Mohan <ramprakash@me.com>
2025-10-05 06:30:11 +0000

    Quickfix and fix statusline

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -20,19 +20,19 @@ M.ui = {
     lazyload = false
   },
   telescope = { style = "bordered" },
-  statusline = {
-    theme = "default", -- default/vscode/vscode_colored/minimal
-    -- default/round/block/arrow separators work only for default statusline theme
-    -- round and block will work for minimal theme only
-    separator_style = "default",
-    order = { "mode", "f", "git", "%=", "lsp_msg", "%=", "lsp",  "cursor", "lines"},
-    modules = {
-      lines = function()
-        return string.format("[%d lines]", vim.fn.line('$'))
-      end,
-      f = "%F"
-    }
-  }
+ statusline = {
+   theme = "default", -- default/vscode/vscode_colored/minimal
+   -- default/round/block/arrow separators work only for default statusline theme
+   -- round and block will work for minimal theme only
+   separator_style = "default",
+   order = { "mode", "f", "git", "%=", "lsp_msg", "%=", "lsp", "cursor", "lines"},
+   modules = {
+     lines = function()
+       return string.format("[%d lines]", vim.fn.line('$'))
+     end,
+     f = "%#St_Mode#%F %#StText#"
+   }
+ }
 }
 
 return M
